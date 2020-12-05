b1bd462ac3dce1c6c10b04c1c3651161f960c33c
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-12-05 08:28:01 -0800

    Add files via upload

---
+++ termfrequencies.py
@@ -0,0 +1,56 @@
+# Counting the frequencies of the terms extracted in the JSON file (accounting for stop words)
+
+import operator 
+import json # necessary for extracting the JSON file containing the Python info
+from collections import Counter # keeping track of all the frequencies
+import re # joining necessary parts like hashtags and emotes together
+
+# Catching for certain emoticons so that it doesn't split
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+# Catching additional keywords (like URLs and hash-tags)
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+ 
+# Allowing spaces to be ignored and catching uppercases/lowercases   
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
+# Catching all tokens in a string and returns as a list 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+# Making sure that emoticons are not lowercased
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+ 
+# Catching the word frequencies for the first tweet extracted from the JSON file
+fname = 'python.json'
+with open(fname, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms
+        terms_all = [term for term in preprocess(tweet['text'])]
+        # Update the counter
+        count_all.update(terms_all)
+    # Print the first 5 most frequent words
+    print(count_all.most_common(5))
