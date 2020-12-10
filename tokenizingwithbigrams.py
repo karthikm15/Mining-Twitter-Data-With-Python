181e69faa8d3bec16a14890350a1b107c99ea015
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-12-10 07:01:57 -0800

    Add files via upload

---
+++ tokenizingwithbigrams.py
@@ -0,0 +1,79 @@
+# Counting the frequencies of the terms extracted in the JSON file (removing stop words)
+
+import operator 
+import json # necessary for extracting the JSON file containing the Python info
+from collections import Counter # keeping track of all the frequencies
+import re # joining necessary parts like hashtags and emotes together
+import string
+from nltk import bigrams # will take a list of tokens and produce a list of tuples using adjacent tokens
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords # explicitly for removing stop words
+
+# Catching for certain emoticons so that it doesn't split
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+# Catching additional keywords (like URLs and hash-tags)
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+ 
+# Allowing spaces to be ignored and catching uppercases/lowercases   
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
+# Catching all tokens in a string and returns as a list 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+# Making sure that emoticons are not lowercased
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+# Making a dictionary of all of the stop words (including rt or retweet and via)
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['rt', 'via']
+# Catching the word frequencies for the first tweet extracted from the JSON file
+fname = 'python.json'
+with open(fname, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms (excluding stop words)
+        terms_stop = [term for term in preprocess(tweet['text']) if term.lower() not in stop]
+        # Update the counter
+        terms_bigram = bigrams(terms_stop)
+        count_all.update(terms_bigram)
+    # Print the first 5 most frequent words
+    print(count_all.most_common(5))
+
+# Finding the most frequent hashtags
+with open(fname, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms (excluding stop words)
+        terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')]
+        # Update the counter
+        terms_bigram = bigrams(terms_hash)
+        count_all.update(terms_bigram)
+    # Print the first 5 most frequent words
+    print(count_all.most_common(5)) 
